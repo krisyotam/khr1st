733471d62577a0698aa7bcc9ec6df8a2fc634edb
Kris Yotam <75515498+krisyotam@users.noreply.github.com>
2025-01-09 14:31:27 -0600

    fix headers

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -47,6 +47,11 @@
 /******/ 	__webpack_require__.m = __webpack_modules__;
 /******/ 	
 /************************************************************************/
+/******/ 	/* webpack/runtime/amd options */
+/******/ 	(() => {
+/******/ 		__webpack_require__.amdO = {};
+/******/ 	})();
+/******/ 	
 /******/ 	/* webpack/runtime/compat get default export */
 /******/ 	(() => {
 /******/ 		// getDefaultExport function for compatibility with non-harmony modules
@@ -125,7 +130,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("5efc418cc529f6e2")
+/******/ 		__webpack_require__.h = () => ("e5bcbf09ee0be902")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c54f127991710dee"; }
+/******/ 		__webpack_require__.h = function() { return "72c643dbf1ed2a7c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
